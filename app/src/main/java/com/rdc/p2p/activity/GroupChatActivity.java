173c3a5986ad745ec88701ff337c59767540995e
wtt0213 <1139572608@qq.com>
2020-06-13 22:11:20 +0800

    聊天界面返回按钮

--- app/src/main/java/com/rdc/p2p/activity/GroupChatActivity.java
+++ app/src/main/java/com/rdc/p2p/activity/GroupChatActivity.java
@@ -3,6 +3,8 @@ package com.rdc.p2p.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.rdc.p2p.R;
@@ -17,16 +19,24 @@ public class GroupChatActivity extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_chat_detail);
         it = getIntent();
+        Button bt = findViewById(R.id.rt_button);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
         initTitle();
     }
 
     protected void initTitle(){
-        //设置标
+        //设置标题
         Bundle bd = it.getExtras();
         groupName = bd.getString("title");
         System.out.println(groupName);
         TextView title = findViewById(R.id.tv_title_group);
         title.setText(groupName);
     }
+
 
 }
